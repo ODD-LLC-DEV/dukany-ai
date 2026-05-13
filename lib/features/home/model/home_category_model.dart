838dec8744f61382ef486c5b6175855aa6128d76
kaspr1907 <abdallahkaspr@gmail.com>
2026-05-14 02:39:50 +0300

    Create home category model

---
+++ lib/features/home/model/home_category_model.dart
@@ -0,0 +1,13 @@
+class HomeCategoryModel {
+  const HomeCategoryModel({
+    required this.id,
+    required this.title,
+    required this.icon,
+    required this.backgroundColor,
+  });
+
+  final int id;
+  final String title;
+  final String icon;
+  final int backgroundColor;
+}

81d87f63fd8423ee0b6a01d413229473663b4c77
kaspr1907 <abdallahkaspr@gmail.com>
2026-05-14 02:51:15 +0300

    Create home category item model

---
+++ lib/features/home/model/home_category_item_model.dart
@@ -0,0 +1,11 @@
+class HomeCategoryItemModel {
+  const HomeCategoryItemModel({
+    required this.id,
+    required this.title,
+    required this.image,
+  });
+
+  final int id;
+  final String title;
+  final String image;
+}
